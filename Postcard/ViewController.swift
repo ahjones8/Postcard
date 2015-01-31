bbd43c2fad66f1f49e354b46df726bd47eabadd8
ahjones8 <ajhussey8@gmail.com>
2015-01-31 15:36:57 -0500

    added a comment to test commits

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -27,6 +27,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //adding comment to test commit
+        
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         
